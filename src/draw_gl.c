5c561e7d33ddbd05893363db91a54282dd3c572e
Ezequiel <ezeq.cruel@gmail.com>
2024-05-09 13:39:02 -0300

    Update draw_gl.c

--- src/draw_gl.c
+++ src/draw_gl.c
@@ -256,7 +256,17 @@ int draw_gl_init (void *data, int clear){ /* init (or de-init) OpenGL */
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, texture, 0);
         
     */
+    /// Build the texture that will serve as the depth attachment for the framebuffer.
+    GLuint depth_texture;
+    glGenTextures(1, &depth_texture);
+    glBindTexture(GL_TEXTURE_2D, depth_texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, gui->gl_ctx.fb_dims[0], gui->gl_ctx.fb_dims[1], 0, GL_DEPTH_COMPONENT, GL_UNSIGNED_BYTE, NULL);
     
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depth_texture, 0);
     
     if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE){
       printf("\nFail to generate frame buffer\n");
